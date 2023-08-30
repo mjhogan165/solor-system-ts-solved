b237a2d5c0813dd6f9529d1a2506b23b1e955761
Matthew Hogan <69777851+mjhogan165@users.noreply.github.com>
2023-08-29 20:32:10 -0400

    without sort()

--- exercises/e16.ts
+++ exercises/e16.ts
@@ -2,13 +2,11 @@
 
 // DO NOT USE `any` for this, you will need to use generics in order to pass both the vitest tests and the tsc compiler
 export function find<T>(array: T[], callback: (arg: T) => boolean) {
-  let result;
   for (const element of array) {
     if (callback(element)) {
-      result = element;
-      break;
+      return element;
     }
   }
-  return result;
+
   // body
 }
--- exercises/e17.ts
+++ exercises/e17.ts
@@ -1,9 +1,29 @@
 export function minBy<T>(array: T[], cb: (arg: T) => number | string) {
-  array.sort((a, b) => (cb(b) > cb(a) ? -1 : 1));
-  return array[0];
+  if (array.length === 0) {
+    return undefined;
+  }
+  let min = array[0];
+  for (let el of array) {
+    if (cb(el) < cb(min as T)) {
+      min = el;
+    }
+  }
+  return min;
+  // array.sort((a, b) => (cb(b) > cb(a) ? -1 : 1));
+  // return array[0];
 }
 
 export function maxBy<T>(array: T[], cb: (arg: T) => number | string) {
-  array.sort((a, b) => (cb(b) < cb(a) ? -1 : 1));
-  return array[0];
+  if (array.length === 0) {
+    return undefined;
+  }
+  let min = array[0];
+  for (const element of array) {
+    if (cb(element) > cb(min as T)) {
+      min = element;
+    }
+  }
+  return min;
+  // array.sort((a, b) => (cb(b) < cb(a) ? -1 : 1));
+  // return array[0];
 }
